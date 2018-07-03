9f46449916c873464ad27392047135a3c1dc2311
Piotr Hebal <phebal@gmail.com>
2018-07-03 11:29:18 -0500

    Define configurable ldapclient attributes Part of: https://github.com/galterlibrary/cd2h-repo-project/issues/40

--- invenio_ldapclient/config.py
+++ invenio_ldapclient/config.py
@@ -7,8 +7,66 @@
 
 """Invenio v3 LDAP client for authentication and user attributes population."""
 
-LDAPCLIENT_EXCLUSIVE_AUTHENTICATION = False
-"""Set to `True' to use LDAP as the authentication method."""
+LDAPCLIENT_AUTHENTICATION = True
+"""Use LDAP as an authentication method without overriding the default."""
+
+LDAPCLIENT_AUTO_REGISTRATION = True
+"""Automatically register users and populate their attributes from LDAP."""
+
+LDAPCLIENT_EXCLUSIVE_AUTHENTICATION = True
+"""
+Set LDAP as the only authentication method, adjust user profile actions,
+remove ability to set custom user attributes. Auto-register user.
+"""
+
+# LDAPCLIENT_SERVER_HOSTNAME = 'example.com'
+"""LDAP server hostname."""
+
+LDAPCLIENT_SERVER_PORT = 389
+"""LDAP server port."""
+
+LDAPCLIENT_USE_SSL = False
+"""Use SSL for LDAP connection."""
+
+LDAPCLIENT_USE_TLS = False
+"""Use TLS for LDAP connection."""
+
+# LDAPCLIENT_ADMIN_ACCOUNT = 'uid=admin,ou=people,dc=example,dc=com'
+"""
+Admin LDAP account used for searching. If not set the authenticating
+user account will be used.
+"""
+
+# LDAPCLIENT_ADMIN_PASSWORD = 'NOTIT'
+"""Admin LDAP account password."""
+
+# LDAPCLIENT_SEARCH_BASE = 'dc=example,dc=com'
+"""Base for binding to LDAP."""
+
+# LDAPCLIENT_BIND_BASE = 'ou=people,dc=example,dc=com'
+"""Base for binding to LDAP."""
+
+LDAPCLIENT_USERNAME_ATTRIBUTE = 'uid'
+"""
+Username LDAP attribute.
+Appended to `LDAPCLIENT_BIND_BASE` with the username from the log-in form
+for binding. For example:
+
+    uid=FORM-USERNAME,ou=people,dc=example,dc=com
+"""
+
+LDAPCLIENT_EMAIL_ATTRIBUTE = 'mail'
+"""Email LDAP attribute."""
+
+LDAPCLIENT_FULL_NAME_ATTRIBUTE = 'displayName'
+"""Full name LDAP attribute."""
+
+LDAPCLIENT_SEARCH_ATTRIBUTES = [
+    LDAPCLIENT_USERNAME_ATTRIBUTE,
+    LDAPCLIENT_EMAIL_ATTRIBUTE,
+    LDAPCLIENT_FULL_NAME_ATTRIBUTE
+]
+"""Attributes to fetch from LDAP."""
 
 LDAPCLIENT_BASE_TEMPLATE = 'invenio_ldapclient/base.html'
 """Default base template for the demo page."""
